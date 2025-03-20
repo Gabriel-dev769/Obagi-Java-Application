1dc4ec01c5552b30a6fa5610787862bbd6ca2cf1
Gabriel-dev769 <gabrielgladdiri9@gmail.com>
2025-03-20 16:19:04 +0100

    UserInput

---
+++ chapter 2/CreateVariables.java
@@ -0,0 +1,7 @@
+public class CreateVariables{
+	public static void main (String[] args){
+		
+	}
+}
+// note before pushing the assignment in chapter two to git hub makesure to type cd.. in your
+// command prompt to make it step out of the chapter two folder
