a210e0667c9267fd04c43b33f79d25edd6896391
Gabriel-dev769 <gabrielgladdiri9@gmail.com>
2025-03-26 16:03:09 +0100

    SwitchStatement

---
+++ IfElseStatement.java
@@ -0,0 +1,13 @@
+public class IfElseStatement{
+	public static void main(String[] args){
+		byte age = 19;
+		
+		if (age >= 18){
+			System.out.printf("You are an adult",age);
+		
+		}
+		else{
+			system.out.printf("your age is %d , you neva reach ")
+		}
+	}
+}
