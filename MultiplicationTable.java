7aab210e7facc23ab0276e5fcb61e70836d217e1
Gabriel😎😎✌ <gabrielgladdiri9@gmail.com>
2025-04-02 16:00:45 +0100

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+
+        
+        do {
+            System.out.print("Enter a positive number: ");
+            number = scanner.nextInt();
+        } while (number <= 0);
+
+        System.out.println("Multiplication Table of " + number);
+
+        
+        int i = 1;
+        while (i <= 12) { 
+            System.out.printf("%d x %d = %d%n", number, i, number * i);
+            i++; 
+        }
+    }
+}
