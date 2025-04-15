bdb41fc7400f21506a0b94ebef59978531790a08
Gabriel😎😎✌ <gabrielgladdiri9@gmail.com>
2025-04-15 16:02:09 +0100

    Add files via upload

---
+++ EnumsInJava.java
@@ -0,0 +1,30 @@
+
+enum AccountType {
+    SAVINGS, CHECKING, FIXED_DEPOSIT;
+}
+
+public class EnumsInJava {
+    public static void main(String[] args) {
+        AccountType type = AccountType.SAVINGS;
+
+        switch (type) {
+            case SAVINGS: {
+                System.out.println("Your saving account has been created");
+            }
+            break;
+
+            case CHECKING: {
+                System.out.println("Your checking account has been created");
+            }
+            break;
+
+            case FIXED_DEPOSIT: {
+                System.out.println("Your fixed deposit account has been created");
+            }
+            break;
+
+            default: 
+                System.out.println("Unknown account type");
+        }
+    }
+}
